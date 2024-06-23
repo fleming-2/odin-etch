a9b3c725f0a13aa74fae4f0b9941dc9ce0bbe570
Liam Fleming <fleming.squared@proton.me>
2024-06-23 22:49:26 +0100

    Implement pad resizing

--- script.js
+++ script.js
@@ -11,22 +11,65 @@ function reset() {
             div.classList.remove('black'));
 }
 
-// Create initial divs
+// Pad side length in px
+const sideLength = 960;
 
-let squareSize = 16;
+// Update no. of squares in pad
+function updatePad(squares) {
+    let container = document.querySelector('.container');
 
-let divContainer = document.getElementsByClassName('container')[0];
+    // live collection of rows
+    let rows = document.getElementsByClassName('row');
 
-for (let i = 0; i < squareSize; i++) {
-    let row = document.createElement('div');
-    for (let j = 0; j < squareSize; j++) {
-        let newDiv = document.createElement('div');
-        newDiv.addEventListener('mouseover', etch);
-        row.appendChild(newDiv);
+    // Remove unnecessary rows
+    while (rows.length > squares) {
+       container.removeChild(container.lastElementChild);
     }
 
-    row.classList.add('row');
-    divContainer.appendChild(row);
+    // Update rows
+    for (let i = 0; i < squares; i++) {
+        let row;
+        if (i < rows.length) {
+            row = rows[i];
+        }
+        else {
+            // Add a new row
+            row = document.createElement('div');
+            row.classList.add('row');
+            container.appendChild(row);
+        }
+
+        // Remove any extra row elements
+        while (row.children.length > squares) {
+            row.removeChild(row.lastElementChild);
+        }
+
+        // Add new row elements as required
+        while (row.children.length < squares) {
+            let newDiv = document.createElement('div');
+            newDiv.addEventListener('mouseover', etch);
+            row.appendChild(newDiv);
+        }
+    }
+
+    reset();
 }
+
+// Create initial divs
+updatePad(16);
+
+// let divContainer = document.getElementsByClassName('container')[0];
+
+// for (let i = 0; i < squareSize; i++) {
+//     let row = document.createElement('div');
+//     for (let j = 0; j < squareSize; j++) {
+//         let newDiv = document.createElement('div');
+//         newDiv.addEventListener('mouseover', etch);
+//         row.appendChild(newDiv);
+//     }
+
+//     row.classList.add('row');
+//     divContainer.appendChild(row);
+// }
 
 document.getElementById('reset').addEventListener('click', reset);
